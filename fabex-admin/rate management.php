76ac4cdfe40c6c6e314f63b695d588714353ae57
Abdqadr1 <abolarinwaquadri@gmail.com>
2021-12-26 07:44:04 +0800

    admin rate page

--- fabex-admin/rate management.php
+++ fabex-admin/rate management.php
@@ -1,1 +1,90 @@
-<h3>Rates</h3>
+<!-- header -->
+<h3 class="text-left mb-3">Rate management</h3>
+<!-- rates -->
+<div class="admin-rate row justify-content-between py-3">
+    <div class="col-6 bordered p-4">
+        <!-- each rate -->
+        <div class="row justify-content-between mt-3 px-4">
+            <div class="col-8 text-muted text-left">
+                <span class="d-inline-block product-name">Crypto</span>
+            </div>
+            <div class="col-2">
+                <input type="number" class="form-control admin-rate text-center">
+            </div>
+        </div>
+        <!-- each rate -->
+        <div class="row justify-content-between mt-3 px-4">
+            <div class="col-8 text-muted text-left">
+                <span class="d-inline-block product-name">Amazon Gift card (200 above)</span>
+            </div>
+            <div class="col-2">
+                <input type="number" class="form-control admin-rate text-center">
+            </div>
+        </div>
+        <!-- each rate -->
+        <div class="row justify-content-between mt-3 px-4">
+            <div class="col-8 text-muted text-left">
+                <span class="d-inline-block product-name">Amazon Gift card (200 above)</span>
+            </div>
+            <div class="col-2">
+                <input type="number" class="form-control admin-rate text-center">
+            </div>
+        </div>
+        <!-- each rate -->
+        <div class="row justify-content-between mt-3 px-4">
+            <div class="col-8 text-muted text-left">
+                <span class="d-inline-block product-name">Crypto</span>
+            </div>
+            <div class="col-2">
+                <input type="number" class="form-control admin-rate text-center">
+            </div>
+        </div>
+        <!-- each rate -->
+        <div class="row justify-content-between mt-3 px-4">
+            <div class="col-8 text-muted text-left">
+                <span class="d-inline-block product-name">Amazon Gift card (200 above)</span>
+            </div>
+            <div class="col-2">
+                <input type="number" class="form-control admin-rate text-center">
+            </div>
+        </div>
+        <!-- each rate -->
+        <div class="row justify-content-between mt-3 px-4">
+            <div class="col-8 text-muted text-left">
+                <span class="d-inline-block product-name">Amazon Gift card (200 above)</span>
+            </div>
+            <div class="col-2">
+                <input type="number" class="form-control admin-rate text-center">
+            </div>
+        </div>
+    </div>
+    <div class="col-6 p-4">
+        <!-- each rate -->
+        <div class="row justify-content-between mt-3 px-4">
+            <div class="col-8 text-muted text-left">
+                <span class="d-inline-block product-name">Crypto</span>
+            </div>
+            <div class="col-2">
+                <input type="number" class="form-control admin-rate text-center">
+            </div>
+        </div>
+        <!-- each rate -->
+        <div class="row justify-content-between mt-3 px-4">
+            <div class="col-8 text-muted text-left">
+                <span class="d-inline-block product-name">Amazon Gift card (200 above)</span>
+            </div>
+            <div class="col-2">
+                <input type="number" class="form-control admin-rate text-center">
+            </div>
+        </div>
+        <!-- each rate -->
+        <div class="row justify-content-between mt-3 px-4">
+            <div class="col-8 text-muted text-left">
+                <span class="d-inline-block product-name">Amazon Gift card (200 above)</span>
+            </div>
+            <div class="col-2">
+                <input type="number" class="form-control admin-rate text-center">
+            </div>
+        </div>
+    </div>
+</div>
--- fabex-admin/settings.php
+++ fabex-admin/settings.php
@@ -1,1 +1,8 @@
-<h2>Settings</h2>
+<div class="row justify-content-center mx-0 p-4">
+    <div class="col-6">
+        <h3>Crypto</h3>
+    </div>
+    <div class="col-6">
+        <h3>Giftcards</h3>
+    </div>
+</div>
