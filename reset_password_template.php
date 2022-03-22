435eed5d32e1542eaac8152f9cb4202bf5e68766
Abdqadr1 <abolarinwaquadri@gmail.com>
2022-03-22 20:24:28 +0800

    email templates fix

--- reset_password_template.php
+++ reset_password_template.php
@@ -7,6 +7,23 @@
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
     <link href="https://fonts.googleapis.com/css2?family=Overpass&display=swap" rel="stylesheet">
     <title>Document</title>
+    <style>
+        img.fabex {
+            display: inline-block;
+            width: 120px;
+            height: 20px;
+        }
+
+        p.address {
+            font-family: "Overpass";
+            font-style: normal;
+            font-weight: 400;
+            font-size: 13px;
+            line-height: 30px;
+            color: #131313;
+            margin-top: 12px;
+        }
+    </style>
 </head>
 
 <body style="padding: 8px 12px; font-family: Overpass, sans-serif;">
@@ -16,9 +33,10 @@
     <p>Hi <em>{FIRST_NAME}</em>,</p>
     <p>You requested to change your password <br> Click the link below to do so</p>
     <a href="{CONFIRMATION_LINK}" style="display: inline-block;">Change password</a>
-    <p style="margin:15px 0px;">Best Regards, <br>Fabex</p>
-    <img src="http://localhost/fabex/assets/images/FabEx3.png" alt="fabex logo">
+    <p style="margin:15px 0px;">Best Regards,</p>
+    <img class="fabex" src="http://localhost/fabex/assets/images/FabEx3.png" alt="fabex logo">
 
+    <p class="address">Suite C2 & C3, Lenu Plaza Sani Abacha,<br /> GRA phase 3, Port Harcourt</p>
     <!-- TODO: confirm the site url-->
     <a href="https://www.fabex.com" style="display: block;margin-top:10px;">https://www.fabex.com</a>
 </body>
--- verify_email_template.php
+++ verify_email_template.php
@@ -7,6 +7,23 @@
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
     <link href="https://fonts.googleapis.com/css2?family=Overpass&display=swap" rel="stylesheet">
     <title>Document</title>
+    <style>
+        img.fabex {
+            display: inline-block;
+            width: 120px;
+            height: 20px;
+        }
+
+        p.address {
+            font-family: "Overpass";
+            font-style: normal;
+            font-weight: 400;
+            font-size: 13px;
+            line-height: 30px;
+            color: #131313;
+            margin-top: 12px;
+        }
+    </style>
 </head>
 
 <body style="padding: 8px 12px; font-family: Overpass, sans-serif;">
@@ -21,9 +38,10 @@
     <h3 style="margin-bottom:20px;margin-left:20px;color:#166d71;text-align:left">Or</h3>
     <p>copy the link below into browser address bar</p>
     <a href="{CONFIRMATION_LINK}" style="margin: 15px 0px;">{CONFIRMATION_LINK}</a>
-    <p style="margin:15px 0px;">Best Regards, <br>Fabex</p>
-    <img src="http://localhost/fabex/assets/images/FabEx3.png" alt="fabex logo">
+    <p style="margin:15px 0px;">Best Regards,</p>
+    <img class="fabex" src="http://localhost/fabex/assets/images/FabEx3.png" alt="fabex logo">
 
+    <p class="address">Suite C2 & C3, Lenu Plaza Sani Abacha,<br /> GRA phase 3, Port Harcourt</p>
     <!-- TODO: confirm the site url-->
     <a href="https://www.fabex.com" style="display: block;margin-top:10px;">https://www.fabex.com</a>
 </body>
